58ff866b37afe6bf703255cc10625f0f5de47990
Kutsan Kaplan <me@kutsankaplan.com>
2022-10-26 23:55:26 +0300

    feat!: use warning level severity

--- src/index.js
+++ src/index.js
@@ -47,31 +47,37 @@ const config = {
       }
     ],
     'order/order': [
-      { type: 'at-rule', name: 'import' },
-      { type: 'at-rule', name: 'forward' },
-      { type: 'at-rule', name: 'use' },
-      'dollar-variables',
-      'at-variables',
-      'custom-properties',
-      { type: 'at-rule', name: 'custom-media' },
-      { type: 'at-rule', name: 'function' },
-      { type: 'at-rule', name: 'mixin' },
-      { type: 'at-rule', name: 'extend' },
-      { type: 'at-rule', name: 'include' },
-      'declarations',
+      [
+        { type: 'at-rule', name: 'import' },
+        { type: 'at-rule', name: 'forward' },
+        { type: 'at-rule', name: 'use' },
+        'dollar-variables',
+        'at-variables',
+        'custom-properties',
+        { type: 'at-rule', name: 'custom-media' },
+        { type: 'at-rule', name: 'function' },
+        { type: 'at-rule', name: 'mixin' },
+        { type: 'at-rule', name: 'extend' },
+        { type: 'at-rule', name: 'include' },
+        'declarations',
+        {
+          type: 'rule',
+          selector: /^&::[\w-]+/,
+          hasBlock: true
+        },
+        'rules',
+        { type: 'at-rule', name: 'media', hasBlock: true }
+      ],
       {
-        type: 'rule',
-        selector: /^&::[\w-]+/,
-        hasBlock: true
-      },
-      'rules',
-      { type: 'at-rule', name: 'media', hasBlock: true }
+        severity: 'warning'
+      }
     ],
     'order/properties-order': [
       propertiesOrder,
       {
         unspecified: 'bottomAlphabetical',
-        emptyLineBeforeUnspecified: 'always'
+        emptyLineBeforeUnspecified: 'always',
+        severity: 'warning'
       }
     ]
   }

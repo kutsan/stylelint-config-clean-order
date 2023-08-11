e27175459441a46cb44babd82ab3256f9dc899a3
Boaz Poolman <boaz_skater@hotmail.com>
2023-08-11 14:36:22 +0200

    feat: Export property groups

--- src/index.js
+++ src/index.js
@@ -1,24 +1,4 @@
-const { positioning } = require('./groups/positioning')
-const { layout } = require('./groups/layout')
-const { boxModel } = require('./groups/box-model')
-const { appearance } = require('./groups/appearance')
-const { typography } = require('./groups/typography')
-const { interaction } = require('./groups/interaction')
-const { transition } = require('./groups/transition')
-const { svgPresentation } = require('./groups/svg-presentation')
-
-const propertyGroups = [
-  ['composes'],
-  ['all'],
-  interaction,
-  positioning,
-  layout,
-  boxModel,
-  typography,
-  appearance,
-  svgPresentation,
-  transition
-]
+const propertyGroups = require('./groups')
 
 const propertiesOrder = propertyGroups.map((properties) => ({
   noEmptyLineBetween: true,

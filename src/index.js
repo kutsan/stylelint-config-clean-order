29ca61afa611617976637bd505eff15377c440fc
Boaz Poolman <boaz_skater@hotmail.com>
2023-08-12 11:19:34 +0200

    Revert "feat: Export property groups"
    This reverts commit e27175459441a46cb44babd82ab3256f9dc899a3.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
-const propertyGroups = require('./groups')
+const { positioning } = require('./groups/positioning')
+const { layout } = require('./groups/layout')
+const { boxModel } = require('./groups/box-model')
+const { appearance } = require('./groups/appearance')
+const { typography } = require('./groups/typography')
+const { interaction } = require('./groups/interaction')
+const { transition } = require('./groups/transition')
+const { svgPresentation } = require('./groups/svg-presentation')
+
+const propertyGroups = [
+  ['composes'],
+  ['all'],
+  interaction,
+  positioning,
+  layout,
+  boxModel,
+  typography,
+  appearance,
+  svgPresentation,
+  transition
+]
 
 const propertiesOrder = propertyGroups.map((properties) => ({
   noEmptyLineBetween: true,
